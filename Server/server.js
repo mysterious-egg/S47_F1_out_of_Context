dec883b4d58d5699461f6a29eb3d8a214c2fa1b0
mysterious-egg <71733395+mysterious-egg@users.noreply.github.com>
2024-02-26 02:53:02 +0530

    Adding the connection code to repo

--- Server/server.js
+++ Server/server.js
@@ -1,17 +1,22 @@
-
 const express = require('express');
-
 const app = express();
-
+const mongoose = require('mongoose');
+const URI = "mongodb+srv://manvesht:jam020130@clustermv.wrduj4w.mongodb.net/?retryWrites=true&w=majority&appName=Clustermv"
 app.get('/ping', (req, res) => {
   res.send('pong');
 });
 
+app.get('/',(req,res)=>{
+  mongoose.connect(URI).then(()=>{
+    res.json({status:"connect"})
+  }).catch((err)=>{
+    res.json({status:"disconnected"})
+  })
+});
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
